7a87fd168d0744efc797787914fba649ecd42c1c
svanmeter93 <107381935+svanmeter93@users.noreply.github.com>
2022-11-30 16:38:34 -0600

    Javascript additions

---
+++ assets/locstr.js
@@ -0,0 +1,15 @@
+// const userInput = document.getElementById("textarea1");
+// const history = document.getElementById("history");
+// const search = document.getElementById("search");
+
+// search.addEventListener("click", display);
+
+// function display() {
+//     // How do I store items into an array?
+//   localStorage.setItem("value", userInput.value);
+//   history.innerHTML = userInput.value;
+// }
+
+// // function createButtons(){
+// // // How do I fetch items out of the array in local storage?
+// // }
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var api = "11b506407df034b36552a6b721f08663";
-
+const singleDayDiv = document.getElementById("single-day")
 // function cityResult(name){
 //     console.log("This is the weather today in " + name +"!")
 // }
@@ -8,7 +8,6 @@ var api = "11b506407df034b36552a6b721f08663";
 // var input = document.getElementById(""
 
 // console.log(userInp);
-
 function userResult(event) {
   event.preventDefault();
   const userSearch = document.getElementById("textarea1").value.trim();
@@ -18,30 +17,32 @@ function userResult(event) {
     .then((data) => {
       const lat = data[0].lat;
       const lon = data[0].lon;
-      //   console.log(data);
+        // console.log(data);
       const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=imperial`;
       fetch(currentURL)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-        const currentWind = data.wind.speed;
-        const currentTemp= data.main.temp;
-        const currentHumidity = data.main.humidity;
-    });
+          // console.log(data);
+          const currentWind = data.wind.speed;
+          const currentTemp = data.main.temp;
+          const currentHumidity = data.main.humidity;
+          const singleDayEl = document.createElement("div");
+          const singleDayContents = `<p>Current Wind: ${currentWind}</p><br><p>`;
+          singleDayEl.innerHTML = singleDayContents;
+          singleDayDiv.append(singleDayEl);
+        });
       const fivedayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api}&units=imperial`;
       fetch(fivedayURL)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          // console.log(data);
           for (let i = 0; i < data.list.length; i += 8) {
             var maxTemp = data.list[i].temp_max;
             var minTemp = data.list[i].temp_min;
-            // console.log(data.list[i]);
+            console.log(data.list[i]);
           }
         });
     });
 }
 
-function createCard(data) {}
-
-search.addEventListener("click", userResult);
+document.getElementById("search").addEventListener("click",userResult)
